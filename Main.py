0793d3e28139c15129f2620a2aa039529961e721
Nauman702 <30662450+Nauman702@users.noreply.github.com>
2023-01-18 15:17:19 +0000

    Day2

--- Main.py
+++ Main.py
@@ -1,10 +1,40 @@
 import mne
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 url="C:\\Users\\nauma\\OneDrive - King's College London\\Research\\Autism Research\\ASD_Sheffield\\ASD Dataset\\EEGLAB"
+#Reading single file
 raw = mne.io.read_raw_eeglab(url+"\\1Abby_Resting.set", preload=True)
 #raw_fdt = mne.io.read_raw_ctf(url+"\\1Abby_Resting.fdt", preload=True)
+
+#Reading all .set files and creating 3D numpy array
+
+eeg_data_list = []
+eeg_ch_list = []
+
+# Loop through all files
+for i in range(1,57):
+    # Read the .set file
+    raw = mne.io.read_raw_eeglab(url+'\\{}Abby_resting.set'.format(i), preload=True)
+
+    # Extract EEG data from the file
+    eeg_data = raw.get_data()
+    # Extract channels data from the file
+    eeg_ch = raw.info ['ch_names']
+    # Append EEG data to the list
+    eeg_data_list.append(eeg_data)
+    # Append channels data to the list
+    eeg_ch_list.append(eeg_ch)
+
+
+# Convert list to 3D numpy array
+eeg_data_3d = np.array(eeg_data_list)
+eeg_ch_3d = np.array(eeg_ch_list)
+
+#------------------Manipulating data----------------
+common_ch = set(eeg_ch_list[0]).intersection(*eeg_ch_list)
+print(common_ch)
+
 
 
 #---------------------EEG Electrodes Data-----------
@@ -47,13 +77,13 @@ montage = mne.channels.make_standard_montage('biosemi64')
 raw.set_montage(montage)
 
 # Plot the montage
-mne.viz.plot_montage(montage, show_names=True, sphere=0.085)
+#mne.viz.plot_montage(montage, show_names=True, sphere=0.085)
 
 #3D?
 
 #-------------------Plotting--------------------------
 # Plot the EEG data
-mne.viz.plot_raw(raw, n_channels=56, color='blue', show_options=True)
+#mne.viz.plot_raw(raw, n_channels=56, color='blue', show_options=True)
 
 # Estimate the power spectral density
 psds, freqs = mne.time_frequency.psd_array_multitaper(eeg_data, fmin=0, fmax=40,sfreq=raw.info['sfreq'])
@@ -64,5 +94,5 @@ for i in range(n_channels):
     plt.plot(freqs, psds[i], label=raw.ch_names[i])
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Power Spectral Density (V^2/Hz)')
-plt.legend()
+plt.legend(ncol=8, loc='upper left')
 plt.show()
